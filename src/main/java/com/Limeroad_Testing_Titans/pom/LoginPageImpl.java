f74a90bf5b871de2f9dc466cf47778bf8616bd1d
Ilakkiyan2010 <ilakkiyanila2010@gmail.com>
2023-10-23 23:44:15 +0530

    pom

--- src/main/java/com/Limeroad_Testing_Titans/pom/LoginPageImpl.java
+++ src/main/java/com/Limeroad_Testing_Titans/pom/LoginPageImpl.java
@@ -1,5 +1,41 @@
 package com.Limeroad_Testing_Titans.pom;
 
-public class LoginPageImpl {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.Limeroad_Testing_Titans.base.BaseClass;
+
+public class LoginPageImpl extends BaseClass {
+	
+	public void FootW_CausalShoe() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy (xpath = "CasualShoeOpt1")
+			private WebElement causalshoe;
+	public WebElement getCausalshoe() {
+		return causalshoe;
+	}
+	
+	@FindBy (xpath = "SlectionOfProduct")
+	private WebElement productSelection;
+	public WebElement getProductSelection() {
+		return productSelection;
+	}
+	
+	@FindBy (xpath = "SelctionSize")
+	private WebElement shoeSize;
+	public WebElement getShoeSize() {
+		return shoeSize;
+	}
+	
+	@FindBy (xpath = "//div[text()='ADD TO CART']")
+	private WebElement addToCart2;
+	public WebElement getAddToCart2() {
+		return addToCart2;
+	}
 }
+
+
+
